1faab0e8939975e66613b1f4c7492b5814e7cd78
Vishal Singh <124653198+VishalSingh1806@users.noreply.github.com>
2025-01-07 13:19:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-SESSION_TIMEOUT = timedelta(hours=1)
+SESSION_TIMEOUT = timedelta(minutes=1)
 
 # Load spaCy model globally during startup
 nlp = spacy_load("en_core_web_sm")
